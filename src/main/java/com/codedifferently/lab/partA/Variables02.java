ffcd52de3ce70aa053b105cc8969c9af4efe737d
Jenna Maslovich <jenna.maslovich@cdpipelinedevshop.com>
2022-10-13 14:52:57 -0400

    part a and b

--- src/main/java/com/codedifferently/lab/partA/Variables02.java
+++ src/main/java/com/codedifferently/lab/partA/Variables02.java
@@ -1,16 +1,17 @@
 package com.codedifferently.lab.partA;
 
 public class Variables02 {
-    public static void main(String[] args) {
-        //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT
-        //Step 1) Change the print statements in lines 9 and 13 so that they print the value of x.
-        //Step 2) Change the value of x to "kitty".
-        String x = "puppy";
-        System.out.println("puppy");
+        public static void main(String[] args) {
+            //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT
+            //Step 1) Change the print statements in lines 12 and 16 so that they print the value of x.
+            //Step 2) Change the value of x to "kitty".
+            String x = "kitty";
+            System.out.println("kitty");
 
-        //Step 3) Add a line of code that changes the value of x to "bunny".
+            //Step 3) Add a line of code that changes the value of x to "bunny".
 
-        System.out.println("puppy");
+            System.out.println("bunny");
 
-    }
+
+        }
 }
--- src/main/java/com/codedifferently/lab/partB/Coin.java
+++ src/main/java/com/codedifferently/lab/partB/Coin.java
@@ -1,4 +1,28 @@
 package com.codedifferently.lab.partB;
 
 public class Coin {
+    private String name;
+    private double value;
+
+    public Coin() {
+        name = "Penny";
+        value = .01;
+    }
+public Coin(String name, double value){
+
+        this.name = name;
+        this.value = value;
 }
+
+public String getName(){return name;}
+    public double getValue(){return value;}
+
+    public String toString(){ return super.toString();
+    }
+
+
+
+    }
+
+
+
